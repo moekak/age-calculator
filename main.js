62eee764f3cca4a17a7a85131a36f086997d1649
moekak <moeka.k0729@gmail.com>
2023-05-23 23:10:27 +0900

    editing

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 const day = document.getElementById("dayInput")
+const month = document.getElementById("monthInput")
+const year = document.getElementById("yearInput")
 const form = document.getElementById("input-bd");
-const value = day.value
+
+
+
 
 form.addEventListener("submit", (e) =>{
-    console.log("aaa");
     e.preventDefault();
-    console.log(dayInput.value);
+     var dayValue = day.value
+     var monthValue = month.value
+     var yearValue = year.value
+    
+    if(isNaN(dayValue) || isNaN(monthValue )|| isNaN(yearValue) || dayValue == "" || monthValue == "" || yearValue == ""){
+        alert("please enter yout birthday")
+    } 
+   
+    
+ 
+
+    const date = new Date();
+    const getYear = date.getFullYear()
+    const getMonth = date.getMonth() + 1
+    const getDate = date.getDate()
+
+
+
+    console.log(yearValue);
+    console.log(monthValue);
+    console.log(dayValue);
 })
