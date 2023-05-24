8f7fed9f780e8e61675257feed767ec5bef9c9f1
moekak <moeka.k0729@gmail.com>
2023-05-24 19:27:10 +0900

    editing

--- main.js
+++ main.js
@@ -2,31 +2,115 @@ const day = document.getElementById("dayInput")
 const month = document.getElementById("monthInput")
 const year = document.getElementById("yearInput")
 const form = document.getElementById("input-bd");
+const errorMessages = document.querySelectorAll(".error")
+const title = document.querySelectorAll(".label")
 
 
-
-
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-     var dayValue = day.value
-     var monthValue = month.value
-     var yearValue = year.value
-    
-    if(isNaN(dayValue) || isNaN(monthValue )|| isNaN(yearValue) || dayValue == "" || monthValue == "" || yearValue == ""){
-        alert("please enter yout birthday")
-    } 
-   
-    
+function handleSubmit(){
+    calculating();
  
+   
+}
 
-    const date = new Date();
-    const getYear = date.getFullYear()
-    const getMonth = date.getMonth() + 1
-    const getDate = date.getDate()
+function calculating(){
+   
+        var dayValue = day.value
+         var monthValue = month.value
+         var yearValue = year.value
+
+         console.log(monthValue);
+    // form.addEventListener("submit", (e) =>{
+    
+    
+
+       
+         var dayValue = day.value
+         var monthValue = month.value
+         var yearValue = year.value
+
+     
+    
+        const date = new Date();
+        const getYear = date.getFullYear()
+        const getMonth = date.getMonth() + 1
+        const getDate = date.getDate()
+         
+        // ==================================　validation ==============================
+     
+        if(dayValue == "" ){
+            errorMessages[0].textContent = "This field is required"
+            title[0].style.color = "hsl(0, 100%, 67%)"
+            day.style.border = "2px solid hsl(0, 100%, 67%)"
+        }  else if(!/^-?\d+$/.test(dayValue) || dayValue < 0 || dayValue > 31){
+            errorMessages[0].textContent = "Must be a valid day"
+            title[0].style.color = "hsl(0, 100%, 67%)"
+            day.style.border = "2px solid hsl(0, 100%, 67%)"
+        }
+        
+        if(monthValue == ""){
+            title[1].style.color = "hsl(0, 100%, 67%)"
+            errorMessages[1].textContent = "This field is required"
+            month.style.border = "2px solid hsl(0, 100%, 67%)"
+        }else if(!/^-?\d+$/.test(monthValue) || monthValue < 1 || monthValue > 12){
+            errorMessages[1].textContent = "Must be a valid day"
+            title[1].style.color = "hsl(0, 100%, 67%)"
+            month.style.border = "2px solid hsl(0, 100%, 67%)"
+        }
+        if(yearValue == ""){
+            title[2].style.color = "hsl(0, 100%, 67%)"
+            errorMessages[2].textContent = "This field is required"
+            year.style.border = "2px solid hsl(0, 100%, 67%)"
+        } else if(!/^-?\d+$/.test(yearValue) || yearValue > getYear){
+            errorMessages[2].textContent = "Must be in the past"
+            title[2].style.color = "hsl(0, 100%, 67%)"
+            year.style.border = "2px solid hsl(0, 100%, 67%)"
+        }
+        // =============================== END ========================================
+    
+       
+    
+        // displaying years
+        const day_border = document.getElementById("borderYear")
+        const month_border = document.getElementById("borderMonth")
+        const year_border = document.getElementById("borderDay")
+    
+    
+     
+        if (errorMessages[0].innerHTML == "" && errorMessages[1].innerHTML == "" && errorMessages[2].innerHTML == ""){
+            var differenceYear = getYear - yearValue;
+            var differenceDate = getDate;
+            var differenceMonth;
+            if(monthValue >= getMonth){
+                differenceMonth=  getMonth + (12 - monthValue)
+            } else{
+                differenceMonth = getMonth - monthValue
+            }
 
 
+            if(dayValue < getDate){
+                differenceDate=  getDate;
+            } else{
+                differenceDate = dayValue - getDate
+            }
+           
+           
+    
+            day_border.innerHTML = differenceYear;
+            month_border.innerHTML = differenceMonth;
+            year_border.innerHTML = differenceDate;
+        }
+      
+       
+       
+        
+        day.value = ""
+        month.value = ""
+        year.value = ""
+    
+    
+       
+    // })
+}
 
-    console.log(yearValue);
-    console.log(monthValue);
-    console.log(dayValue);
-})
+  
+
