1e8d39b05291c68fbd8550e29a32dd3ef3e9d599
moekak <moeka.k0729@gmail.com>
2023-05-25 16:02:13 +0900

    editing

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const form = document.getElementById("input-bd");
 const errorMessages = document.querySelectorAll(".error")
 const title = document.querySelectorAll(".label")
 
+// displaying years
+const day_border = document.getElementById("borderYear")
+const month_border = document.getElementById("borderMonth")
+const year_border = document.getElementById("borderDay")
+
 
 function handleSubmit(){
     calculating();
@@ -13,125 +18,146 @@ function handleSubmit(){
 }
 
 function calculating(){
+
+
+    
+    const date = new Date();
+    const ms = date.getTime()
+
+
+    const getYear = date.getFullYear();
+
    
-        var dayValue = day.value
-         var monthValue = month.value
-         var yearValue = year.value
 
-    
-    // form.addEventListener("submit", (e) =>{
-    
-    
-
-       
-         var dayValue = day.value
-         var monthValue = month.value
-         var yearValue = year.value
+    var dayValue = day.value
+    var monthValue = month.value
+    var yearValue = year.value
 
      
     
-        let date = new Date();
-        date = date.getTime()
 
-        console.log(date);
-        let bdDate =  new Date(yearValue, monthValue , dayValue)
+     
+
+
+    
+        let bdDate =  new Date(yearValue, monthValue -1 , dayValue)
         bdDate = bdDate.getTime()
-        const diffs = date - bdDate
+
+     
+
+
+
+
+        const diffs = ms - bdDate
+      
         const diffTime = diffs / 1000/60/60/24/365.24
-        console.log(diffTime);
+        const diffMonth = (diffTime - Math.floor(diffTime)) * 12
+        const diffDay = (diffMonth - Math.floor(diffMonth)) * 30.44
+    
+
+        const finalDiffYear = Math.floor(diffTime)
+        const finalDiffMonth = Math.floor(diffMonth)
+        const finalDiffDay = Math.floor(diffDay)
 
 
-        const diff = date - bdDate;
 
-        // 分に変換
-        const diffMin = diffTime / 60000 
-        // 時間に変更
-        const diffHour = diffTime / 60000 / 60
-        // 日付に変更
-        const diffDay = diffTime / 60000 / 60/ 24
-        // 月に変換
-        const diffMonth = diffTime / 60000 / 60/ 24 / 30.4167 
-        console.log(diffMonth);
-        // 年に変換
-        const diffYear = (diffMonth / 12)
-        const bb = Math.floor(diffYear)
-        const moeka = (diffYear - Math.floor(diffYear)) * 12
-        const dean = Math.floor(moeka)
-        const aaa = Math.floor((moeka - dean) * 30.44)
-        console.log(bb);
-        console.log(dean);
-        console.log(aaa);
-        // const bdDay = bdDate.getDate()
-        // const bdMonth = bdDate.getMonth()
-        // const bdYear = bdDate.getYear()
-        // console.log(bdDate);
-        // console.log(bdDay);
-        // console.log(bdMonth);
-        // console.log(bdYear);
+
          
         // ==================================　validation ==============================
+
+     
+
+
+    if(diffs < 0){
+        errorMessages[0].textContent = "Must be a valid day"
+        errorMessages[1].textContent = "Must be a valid day"
+        errorMessages[2].textContent = "Must be a valid day"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
      
         if(dayValue == "" ){
             errorMessages[0].textContent = "This field is required"
             title[0].style.color = "hsl(0, 100%, 67%)"
             day.style.border = "2px solid hsl(0, 100%, 67%)"
-        }  else if(!/^-?\d+$/.test(dayValue) || dayValue < 0 || dayValue > 31){
+        }  else if(!/^-?\d+$/.test(dayValue) || dayValue < 0 || dayValue > 31 ){
             errorMessages[0].textContent = "Must be a valid day"
             title[0].style.color = "hsl(0, 100%, 67%)"
             day.style.border = "2px solid hsl(0, 100%, 67%)"
+        } else if(diffs < 0){
+            errorMessages[0].textContent = "Must be a valid date"
+            title[0].style.color = "hsl(0, 100%, 67%)"
+            day.style.border = "2px solid hsl(0, 100%, 67%)"
+
+        } else{
+            errorMessages[0].textContent = ""
+            day.style.border = "1px solid rgba(0, 0, 0, 0.411)"
+            title[0].style.color = "hsl(0, 1%, 44%)"
         }
+        
         
         if(monthValue == ""){
             title[1].style.color = "hsl(0, 100%, 67%)"
             errorMessages[1].textContent = "This field is required"
             month.style.border = "2px solid hsl(0, 100%, 67%)"
-        }else if(!/^-?\d+$/.test(monthValue) || monthValue < 1 || monthValue > 12){
-            errorMessages[1].textContent = "Must be a valid day"
+        }else if(!/^-?\d+$/.test(monthValue) || monthValue < 1 || monthValue > 12) {
+            errorMessages[1].textContent = "Must be a valid month"
             title[1].style.color = "hsl(0, 100%, 67%)"
             month.style.border = "2px solid hsl(0, 100%, 67%)"
+        } else if(diffs < 0){
+            title[1].style.color = "hsl(0, 100%, 67%)"
+            month.style.border = "2px solid hsl(0, 100%, 67%)"
+            errorMessages[1].textContent = ""
+        } else{
+            errorMessages[1].textContent = ""
+            month.style.border = "1px solid rgba(0, 0, 0, 0.411)"
+            title[1].style.color = "hsl(0, 1%, 44%)"
         }
+       
+
+        
         if(yearValue == ""){
             title[2].style.color = "hsl(0, 100%, 67%)"
             errorMessages[2].textContent = "This field is required"
             year.style.border = "2px solid hsl(0, 100%, 67%)"
-        } else if(!/^-?\d+$/.test(yearValue) || yearValue > getYear){
-            errorMessages[2].textContent = "Must be in the past"
+        } else if(!/^-?\d+$/.test(yearValue) ){
+            errorMessages[2].textContent = "Must be a valid year"
             title[2].style.color = "hsl(0, 100%, 67%)"
             year.style.border = "2px solid hsl(0, 100%, 67%)"
+        } else if(diffs < 0){
+            title[2].style.color = "hsl(0, 100%, 67%)"
+            year.style.border = "2px solid hsl(0, 100%, 67%)"
+            errorMessages[2].textContent = ""
+        }else{
+            errorMessages[2].textContent = ""
+            year.style.border = "1px solid rgba(0, 0, 0, 0.411)"
+            title[2].style.color = "hsl(0, 1%, 44%)"
         }
+        
         // =============================== END ========================================
     
        
     
-        // displaying years
-        const day_border = document.getElementById("borderYear")
-        const month_border = document.getElementById("borderMonth")
-        const year_border = document.getElementById("borderDay")
-    
+        
     
      
         if (errorMessages[0].innerHTML == "" && errorMessages[1].innerHTML == "" && errorMessages[2].innerHTML == ""){
-            var differenceYear = getYear - yearValue;
-            var differenceDate = getDate;
-            var differenceMonth;
-            if(monthValue > getMonth){
-                differenceMonth=  getMonth + (12 - monthValue)
-            } else{
-                differenceMonth = getMonth - monthValue
-            }
-
-
-            if(dayValue < getDate){
-                differenceDate=  getDate;
-            } else{
-                differenceDate = dayValue - getDate
-            }
-           
            
     
-            day_border.innerHTML = differenceYear;
-            month_border.innerHTML = differenceMonth;
-            year_border.innerHTML = differenceDate;
+            day_border.innerHTML = finalDiffYear;
+            month_border.innerHTML = finalDiffMonth;
+            year_border.innerHTML = finalDiffDay;
         }
       
        
@@ -141,7 +167,9 @@ function calculating(){
         month.value = ""
         year.value = ""
     
-    
+    console.log(errorMessages[0]);
+    console.log(errorMessages[1]);
+    console.log(errorMessages[2]);
        
     // })
 }
